c905b93a4fd7ef605cb6192cf9758493903a4eaa
Edelmeyder <edelmiro.7.8.9.5@gmail.com>
2022-02-12 15:12:10 -0300

    new lcd library

--- simulation/STM32F103C6/LCD.c
+++ simulation/STM32F103C6/LCD.c
@@ -1,85 +1,178 @@
-#include "LCD.h"
+#include "lcd.h"
 
-// cursor position to DDRAM mapping
-#define LCD_LINE0_DDRAMADDR		0x00
-#define LCD_LINE1_DDRAMADDR		0x40
-
-
-
-void LCD_Init(void) {
-	// esperar 15ms luego de encendar para inicializar el lcd
-	Utils_Delay_us(15000);
-	// activa el clock del puerto del lcd
-	RCC->APB2ENR |= LCDIOCLK;
-	// establece todos los pines del lcd como salida
-	LCDPORT->ODR &= ~(1<<LCD_D0 | 1<<LCD_D1 | 1<<LCD_D2 | 1<<LCD_D3 | 1<<LCD_D4 | 1<<LCD_D5 | 1<<LCD_D6 | 1<<LCD_D7 | 1<<LCD_RW | 1<<LCD_RS | 1<<LCD_E);
-	LCDPORT->CRH |= 3<<(LCD_D0-8)*4 | 3<<(LCD_D1-8)*4 | 3<<(LCD_D2-8)*4 | 3<<(LCD_D3-8)*4 | 3<<(LCD_D4-8)*4 | 3<<(LCD_D5-8)*4 | 3<<(LCD_D6-8)*4 | 3<<(LCD_D7-8)*4;
-	//LCDPORT->CRH &= ~(12<<(LCD_D0-8)*4 | 12<<(LCD_D1-8)*4 | 12<<(LCD_D2-8)*4 | 12<<(LCD_D3-8)*4 | 12<<(LCD_D4-8)*4 | 12<<(LCD_D5-8)*4 | 12<<(LCD_D6-8)*4 | 12<<(LCD_D7-8)*4);
-	LCDPORT->CRL |= 3<<LCD_RW*4 | 3<<LCD_RS*4 | 3<<LCD_E*4;
-	//LCDPORT->CRL &= ~(12<<LCD_RW*4 | 12<<LCD_RS*4 | 12<<LCD_E*4);
-	Utils_Delay_us(4200);
-	LCD_sendCommand(0x38);
-	LCD_sendCommand(0x0E);
-	
-	LCD_sendCommand(0x01);
-	Utils_Delay_us(2000);
-	LCD_sendCommand(0x06);
-}
+void delay_us(uint16_t);
+void load_data(uint8_t);
 
 void LCD_sendCommand(uint8_t cmd){
-	LCDPORT->ODR &= ~(1<<LCD_RS);
-	LCDPORT->ODR |= cmd<<8;
-	LCDPORT->ODR &= (0xFFFF&(0x00FF|(cmd<<8)));
-	LCDPORT->ODR |= 1<<LCD_E;
-	Utils_Delay_us(2);
-	LCDPORT->ODR &= ~(1<<LCD_E);
-	if ((cmd == 1) | (cmd == 2) | (cmd == 3))
-		Utils_Delay_us(2000);
+	LCDPORT_CMD->BRR = (1<<LCD_RS | 1<<LCD_RW);
+	load_data(cmd);
+	LCDPORT_CMD->BSRR = (1<<LCD_E);
+	delay_us(1);
+	LCDPORT_CMD->BRR = (1<<LCD_E);
+	if (cmd <= 3)
+		delay_us(2000);
 	else
-		Utils_Delay_us(100);
+		delay_us(50);
 }
 
-void LCD_sendChar(uint8_t c)
-{
-	LCDPORT->ODR |= 1<<LCD_RS;
-	LCDPORT->ODR |= c<<8;
-	LCDPORT->ODR &= (0xFFFF&(0x00FF|(c<<8)));
-	LCDPORT->ODR |= 1<<LCD_E;
-	Utils_Delay_us(2);
-	LCDPORT->ODR &= ~(1<<LCD_E);
-	Utils_Delay_us(100);
-	LCDPORT->ODR &= ~(1<<LCD_RS);
+void LCD_sendChar(uint8_t ch) {
+	LCDPORT_CMD->BSRR = (1<<LCD_RS | 1<<(LCD_RW + 16));
+	load_data(ch);
+	LCDPORT_CMD->BSRR = (1<<LCD_E);
+	delay_us(1);
+	LCDPORT_CMD->BRR = (1<<LCD_E);
+	delay_us(50);
+	LCDPORT_CMD->BRR = (1<<LCD_RS);
 }
 
-void LCD_sendString(uint8_t* string, uint8_t length)
-{
+void LCD_init(){
+	// Made as per the HD44780 datasheet
+	delay_us(50000); // Wait more than 40 ms
+	
+	RCC->APB2ENR |= LCDIOCLK_CMD | LCDIOCLK_DATA; // Starts clock for LCD port/s
+	
+	LCDPORT_CMD->BRR = (1<<LCD_RS | 1<<LCD_RW | 1<<LCD_E); // Pins to 0
+	// Sets pins as outputs
+#if LCDPIN_CMD_POS 
+	LCDPORT_CMD->CRL = ((LCDPORT_CMD->CRL & ~(12<<LCD_RS*4 | 12<<LCD_RW*4 | 12<<LCD_E*4)) | (3<<LCD_RS*4 | 3<<LCD_RW*4 | 3<<LCD_E*4));
+#else
+	LCDPORT_CMD->CRH = ((LCDPORT_CMD->CRH & ~(12<<(LCD_RS-8)*4 | 12<<(LCD_RW-8)*4 | 12<<(LCD_E-8)*4)) | (3<<(LCD_RS-8)*4 | 3<<(LCD_RW-8)*4 | 3<<(LCD_E-8)*4));
+#endif
+	
+	LCDPORT_DATA->BRR = (1<<LCD_D7 | 1<<LCD_D6 | 1<<LCD_D5 | 1<<LCD_D4); // Pins to 0
+	// Sets pins as outputs
+#if LCDPIN_DATA_POS
+	LCDPORT_DATA->CRL = ((LCDPORT_DATA->CRL 
+		& ~(12<<LCD_D4*4 | 12<<LCD_D5*4 | 12<<LCD_D6*4 | 12<<LCD_D7*4)) 
+		| (3<<LCD_D4*4 | 3<<LCD_D5*4 | 3<<LCD_D6*4 | 3<<LCD_D7*4));
+#else
+	LCDPORT_DATA->CRH = ((LCDPORT_DATA->CRH 
+		& ~(12<<(LCD_D4-8)*4 | 12<<(LCD_D5-8)*4 | 12<<(LCD_D6-8)*4 | 12<<(LCD_D7-8)*4)) 
+		| (3<<(LCD_D4-8)*4 | 3<<(LCD_D5-8)*4 | 3<<(LCD_D6-8)*4 | 3<<(LCD_D7-8)*4));
+#endif
+	//-----One---------
+	LCDPORT_DATA->BSRR = (1<<LCD_D5 | 1<<LCD_D4 | 1<<(LCD_D7 + 16) | 1<<(LCD_D6 + 16));
+	LCDPORT_CMD->BRR = (1<<LCD_RS | 1<<LCD_RW);
+	LCDPORT_CMD->BSRR = (1<<LCD_E);
+	delay_us(1);
+	LCDPORT_CMD->BRR = (1<<LCD_E);
+	delay_us(4500); // Wait more than 4.2 ms
+	
+	//-----Two---------
+	LCDPORT_DATA->BSRR = (1<<LCD_D5 | 1<<LCD_D4 | 1<<(LCD_D7 + 16) | 1<<(LCD_D6 + 16));
+	LCDPORT_CMD->BRR = (1<<LCD_RS | 1<<LCD_RW);
+	LCDPORT_CMD->BSRR = (1<<LCD_E);
+	delay_us(1);
+	LCDPORT_CMD->BRR = (1<<LCD_E);
+	delay_us(150); // Wait more than 100 us
+	
+	//-----Three-------
+	LCDPORT_DATA->BSRR = (1<<LCD_D5 | 1<<LCD_D4 | 1<<(LCD_D7 + 16) | 1<<(LCD_D6 + 16));
+	LCDPORT_CMD->BRR = (1<<LCD_RS | 1<<LCD_RW);
+	LCDPORT_CMD->BSRR = (1<<LCD_E);
+	delay_us(1);
+	LCDPORT_CMD->BRR = (1<<LCD_E);
+	delay_us(50);
+#ifdef LCD_4bit // 4-bit part
+	LCDPORT_DATA->BSRR = (1<<LCD_D5 | 1<<(LCD_D4 + 16) | 1<<(LCD_D7 + 16) | 1<<(LCD_D6 + 16));
+	LCDPORT_CMD->BRR = (1<<LCD_RS | 1<<LCD_RW);
+	LCDPORT_CMD->BSRR = (1<<LCD_E);
+	delay_us(1);
+	LCDPORT_CMD->BRR = (1<<LCD_E);
+	delay_us(1);
+	// Set interface 4-bit 2 line display
+	LCD_sendCommand(0x28);
+#else // 8-bit part
+	LCDPORT_DATA->BRR = (1<<LCD_D3 | 1<<LCD_D2 | 1<<LCD_D1 | 1<<LCD_D0); // Pins to 0
+	// Sets pins as outputs
+	#if LCDPIN_DATA_POS
+		LCDPORT_DATA->CRL = ((LCDPORT_DATA->CRL 
+			& ~(12<<LCD_D0*4 | 12<<LCD_D1*4 | 12<<LCD_D2*4 | 12<<LCD_D3*4)) 
+			| (3<<LCD_D0*4 | 3<<LCD_D1*4 | 3<<LCD_D2*4 | 3<<LCD_D3*4));
+	#else
+		LCDPORT_DATA->CRH = ((LCDPORT_DATA->CRH 
+			& ~(12<<(LCD_D0-8)*4 | 12<<(LCD_D1-8)*4 | 12<<(LCD_D2-8)*4 | 12<<(LCD_D3-8)*4)) 
+			| (3<<(LCD_D0-8)*4 | 3<<(LCD_D1-8)*4 | 3<<(LCD_D2-8)*4 | 3<<(LCD_D3-8)*4));
+	#endif
+	// Set interface 8-bit 2 line display
+	LCD_sendCommand(0x38);
+#endif
+	// Visible display - increment address - cursor off
+	LCD_sendCommand(0x0C);
+}
+
+void LCD_sendString(uint8_t* st, uint8_t l){
 	uint8_t i ;
-	for (i= 0; i < length; i++) {
-		LCD_sendChar(string[i]);
+	for (i= 0; i < l; i++) {
+		LCD_sendChar(st[i]);
 	}
 }
 
-void LCD_PrintString(uint8_t* str)
-{
-	//LCD_goToXY(0,0);
-	uint8_t i;
-	i = 0;
-	while(str[i] != 0)
-	{
-		LCD_sendChar(str[i]);
-	}
-}
-
-void LCD_goToXY(uint8_t x, uint8_t y) {
+void LCD_goToXY(uint8_t x, uint8_t y){ //Cursor to X Y position
 	register uint8_t DDRAMAddr;
 	// remap lines into proper order
-	switch(y) {
+	switch(y){
 		case 0: DDRAMAddr = LCD_LINE0_DDRAMADDR+x; break;
 		case 1: DDRAMAddr = LCD_LINE1_DDRAMADDR+x; break;
+		case 2: DDRAMAddr = LCD_LINE2_DDRAMADDR+x; break;
+		case 3: DDRAMAddr = LCD_LINE3_DDRAMADDR+x; break;
 		default: DDRAMAddr = LCD_LINE0_DDRAMADDR+x;
 	}
 	// set data address
-	LCD_sendCommand(1<<7 | DDRAMAddr);
+	LCD_sendCommand(1<<LCD_DDRAM | DDRAMAddr);	
 }
 
+void LCD_clear(void) { //Clears LCD
+	LCD_sendCommand(1<<LCD_CLR);
+}
 
+void LCD_cursorOn(void){ //displays LCD cursor
+	LCD_sendCommand(0x0E);
+}
+
+void LCD_cursorOnBlink(void){ //displays LCD blinking cursor
+	LCD_sendCommand(0x0F);
+}
+
+void LCD_cursorOFF(void){ //turns OFF cursor
+	LCD_sendCommand(0x0C);
+}
+
+void LCD_visible(void){ //Shows LCD
+	LCD_sendCommand(0x0C);
+}
+
+void load_data(uint8_t data) {
+#ifdef LCD_4bit // 4-bit part
+	// High
+	LCDPORT_DATA->BSRR = (1<<((data & (1<<7))? LCD_D7 : (LCD_D7 + 16)));
+	LCDPORT_DATA->BSRR = (1<<((data & (1<<6))? LCD_D6 : (LCD_D6 + 16)));
+	LCDPORT_DATA->BSRR = (1<<((data & (1<<5))? LCD_D5 : (LCD_D5 + 16)));
+	LCDPORT_DATA->BSRR = (1<<((data & (1<<4))? LCD_D4 : (LCD_D4 + 16)));
+	LCDPORT_CMD->BSRR = (1<<LCD_E);
+	delay_us(1);
+	LCDPORT_CMD->BRR = (1<<LCD_E);
+	delay_us(1);
+
+	//Low
+	LCDPORT_DATA->BSRR = (1<<((data & (1<<3))? LCD_D7 : (LCD_D7 + 16)));
+	LCDPORT_DATA->BSRR = (1<<((data & (1<<2))? LCD_D6 : (LCD_D6 + 16)));
+	LCDPORT_DATA->BSRR = (1<<((data & (1<<1))? LCD_D5 : (LCD_D5 + 16)));
+	LCDPORT_DATA->BSRR = (1<<((data & (1<<0))? LCD_D4 : (LCD_D4 + 16)));
+#else // 8-bit part
+	LCDPORT_DATA->BSRR = (1<<((data & (1<<7))? LCD_D7 : (LCD_D7 + 16)));
+	LCDPORT_DATA->BSRR = (1<<((data & (1<<6))? LCD_D6 : (LCD_D6 + 16)));
+	LCDPORT_DATA->BSRR = (1<<((data & (1<<5))? LCD_D5 : (LCD_D5 + 16)));
+	LCDPORT_DATA->BSRR = (1<<((data & (1<<4))? LCD_D4 : (LCD_D4 + 16)));
+	LCDPORT_DATA->BSRR = (1<<((data & (1<<3))? LCD_D3 : (LCD_D3 + 16)));
+	LCDPORT_DATA->BSRR = (1<<((data & (1<<2))? LCD_D2 : (LCD_D2 + 16)));
+	LCDPORT_DATA->BSRR = (1<<((data & (1<<1))? LCD_D1 : (LCD_D1 + 16)));
+	LCDPORT_DATA->BSRR = (1<<((data & (1<<0))? LCD_D0 : (LCD_D0 + 16)));
+#endif
+}
+
+void delay_us(uint16_t t){ // Dalay works for CPU_freq = 72MHz
+	uint16_t l, i;
+	for(i = 0; i < t; i++)
+		for(l = 0; l < 12; l++){}
+}
--- simulation/STM32F103C6/LCD.h
+++ simulation/STM32F103C6/LCD.h
@@ -1,34 +1,73 @@
-#ifndef _LCD_H
-#define _LCD_H
-
-#include <stm32f103x6.h>                 // Device header
+#ifndef LCD_H
+#define LCD_H
 
 #include <stdint.h>
+#include <stm32f103x6.h>                  // Device header
 
-#include "lcd.h"
-#include "utils.h"
+//Uncomment this if LCD 4 bit interface is used
+#define LCD_4bit
 
-#define LCDPORT GPIOB //puerto al que se conecta el lcd
-#define LCDIOCLK RCC_APB2ENR_IOPBEN
+#define LCDPORT_DATA GPIOB
+#define LCDIOCLK_DATA RCC_APB2ENR_IOPBEN
+#define LCDPORT_CMD GPIOB
+#define LCDIOCLK_CMD RCC_APB2ENR_IOPBEN
 
-#define LCD_D0	8	//pin conectado a LCD D0
-#define LCD_D1	9	//pin conectado a LCD D1
-#define LCD_D2	10	//pin conectado a LCD D2
-#define LCD_D3	11	//pin conectado a LCD D3
-#define LCD_D4	12	//pin conectado a LCD D4
-#define LCD_D5	13	//pin conectado a LCD D5
-#define LCD_D6	14	//pin conectado a LCD D6
-#define LCD_D7	15	//pin conectado a LCD D7
-#define LCD_RW	6 //pin conectado a LCD R/W
-#define LCD_RS	7 //pin conectado a LCD RS
-#define LCD_E	5	//pin conectado a LCD E
+//Define LCDPIN_XXX_POS to 1 if low pins (0-7) are been used
+//Define LCDPIN_XXX_POS to 0 if high pins (8-15) are been used
+#define LCDPIN_DATA_POS 1
+#define LCDPIN_CMD_POS 0
 
-void LCD_Init(void);
-void LCD_sendCommand(uint8_t);
-void LCD_goToXY(uint8_t, uint8_t);
-void LCD_sendChar(uint8_t);
-void LCD_sendString(uint8_t*, uint8_t);
-void LCD_clear(void);
-void LCD_PrintString(uint8_t* str);
+#define LCD_D0	0	//define MCU pin connected to LCD D0
+#define LCD_D1	1	//define MCU pin connected to LCD D1
+#define LCD_D2	2	//define MCU pin connected to LCD D1
+#define LCD_D3	3	//define MCU pin connected to LCD D2
+#define LCD_D4	4	//define MCU pin connected to LCD D3
+#define LCD_D5	2	//define MCU pin connected to LCD D4
+#define LCD_D6	6	//define MCU pin connected to LCD D5
+#define LCD_D7	3	//define MCU pin connected to LCD D6
+#define LCD_RS	12	//define MCU pin connected to LCD RS
+#define LCD_RW	11	//define MCU pin connected to LCD R/W (If RW is grounded define to the same value as RS)
+#define LCD_E	10	//define MCU pin connected to LCD E
+
+//commands
+#define LCD_CLR             0	//DB0: clear display
+#define LCD_HOME            1	//DB1: return to home position
+#define LCD_ENTRY_MODE      2	//DB2: set entry mode
+#define LCD_ENTRY_INC       1	//DB1: increment
+#define LCD_ENTRY_SHIFT     0	//DB2: shift
+#define LCD_ON_CTRL         3	//DB3: turn lcd/cursor on
+#define LCD_ON_DISPLAY      2	//DB2: turn display on
+#define LCD_ON_CURSOR       1	//DB1: turn cursor on
+#define LCD_ON_BLINK        0	//DB0: blinking cursor
+#define LCD_MOVE            4	//DB4: move cursor/display
+#define LCD_MOVE_DISP       3	//DB3: move display (0-> move cursor)
+#define LCD_MOVE_RIGHT      2	//DB2: move right (0-> left)
+#define LCD_FUNCTION        5	//DB5: function set
+#define LCD_FUNCTION_8BIT   4	//DB4: set 8BIT mode (0->4BIT mode)
+#define LCD_FUNCTION_2LINES 3	//DB3: two lines (0->one line)
+#define LCD_FUNCTION_10DOTS 2	//DB2: 5x10 font (0->5x7 font)
+#define LCD_CGRAM           6	//DB6: set CG RAM address
+#define LCD_DDRAM           7	//DB7: set DD RAM address
+// reading:
+#define LCD_BUSY            7	//DB7: LCD is busy
+#define LCD_LINES						2	//visible lines
+#define LCD_LINE_LENGTH		16	//line length (in characters)
+// cursor position to DDRAM mapping
+#define LCD_LINE0_DDRAMADDR		0x00
+#define LCD_LINE1_DDRAMADDR		0x40
+#define LCD_LINE2_DDRAMADDR		0x14
+#define LCD_LINE3_DDRAMADDR		0x54
+
+//Functions
+void LCD_init(void); //Initializes LCD
+void LCD_sendCommand(uint8_t); //Sends command to LCD
+void LCD_sendChar(uint8_t); //Sends character to LCD
+void LCD_sendString(uint8_t*, uint8_t); //Sends string to LCD
+void LCD_goToXY(uint8_t, uint8_t); //Cursor to X Y position
+void LCD_clear(void); //Clears LCD
+void LCD_cursorOn(void);		//Underline cursor ON
+void LCD_cursorOnBlink(void);	//Underline blinking cursor ON
+void LCD_cursorOFF(void);		//Cursor OFF
+void LCD_visible(void);			//LCD visible
 
 #endif
