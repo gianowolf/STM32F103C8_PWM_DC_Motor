632234b0d0149004b92c67e7ec0634a87b4c0bbe
Edelmeyder <edelmiro.7.8.9.5@gmail.com>
2022-02-12 20:43:38 -0300

    written leds module

--- simulation/STM32F103C6/Leds.c
+++ simulation/STM32F103C6/Leds.c
@@ -1,14 +1,32 @@
 #include "Leds.h"
 
-void Leds_Init(void)
-{
-
+void Leds_Init(void) {
+	LED_G_pin > 7 ? LED_G_port->CRH = (LED_G_port->CRH | (3<<((LED_G_pin - 8)*4))) & (0xFFFFFFFF & (3<<((LED_G_pin - 8)*4))) 
+					: LED_G_port->CRL = (LED_G_port->CRL | (3<<(LED_G_pin*4))) & (0xFFFFFFFF & (3<<(LED_G_pin*4)));
+	LED_Y_pin > 7 ? LED_Y_port->CRH = (LED_Y_port->CRH | (3<<((LED_Y_pin - 8)*4))) & (0xFFFFFFFF & (3<<((LED_Y_pin - 8)*4))) 
+					: LED_Y_port->CRL = (LED_Y_port->CRL | (3<<(LED_Y_pin*4))) & (0xFFFFFFFF & (3<<(LED_Y_pin*4)));
+	LED_R_pin > 7 ? LED_R_port->CRH = (LED_R_port->CRH | (3<<((LED_R_pin - 8)*4))) & (0xFFFFFFFF & (3<<((LED_R_pin - 8)*4))) 
+					: LED_R_port->CRL = (LED_R_port->CRL | (3<<(LED_R_pin*4))) & (0xFFFFFFFF & (3<<(LED_R_pin*4)));
+	LED_G_port->BRR = (1<<LED_G_pin);
+	LED_Y_port->BRR = (1<<LED_Y_pin);
+	LED_R_port->BRR = (1<<LED_R_pin);
+	// Sets led pins as outputs
 }
 
-void Leds_WaterLevel_Off(void)
-{
+void Leds_WaterLevel_Normal(void) { // Turns off red and yellow, turns on green
+	LED_R_port->BRR = (1<<LED_R_pin);
+	LED_Y_port->BRR = (1<<LED_Y_pin);
+	LED_G_port->BSRR = (1<<LED_G_pin);
 }
 
-void Leds_WaterLevel_On(void)
-{
+void Leds_WaterLevel_Low(void) { // Turns off red and green, turns on yellow
+	LED_R_port->BRR = (1<<LED_R_pin);
+	LED_G_port->BRR = (1<<LED_G_pin);
+	LED_Y_port->BSRR = (1<<LED_Y_pin);
+}
+
+void Leds_WaterLevel_Empty(void) { // Turns off green and yellow, turns on red
+	LED_G_port->BRR = (1<<LED_G_pin);
+	LED_Y_port->BRR = (1<<LED_Y_pin);
+	LED_R_port->BSRR = (1<<LED_R_pin);
 }
--- simulation/STM32F103C6/Leds.h
+++ simulation/STM32F103C6/Leds.h
@@ -1,6 +1,13 @@
 #ifndef _LEDS_H
 #define _LEDS_H
 
+#define LED_G_port GPIOA
+#define LED_G_pin 15
+#define LED_Y_port GPIOA
+#define LED_Y_pin 12
+#define LED_R_port GPIOA
+#define LED_R_pin 11
+
 void Leds_Init(void);
 void Leds_WaterLevel_Normal(void);
 void Leds_WaterLevel_Low(void);
